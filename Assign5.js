bde84e87d6b0052db37a8819e1b11a40058cc888
Margaret Wismer <margaretgwismer.gmail.com>
2016-11-16 15:41:49 -0500

    Initial commit

---
+++ Assign5.js
@@ -0,0 +1,8 @@
+  window.addEventListener("load",setTimeout(displayModal,3000));  
+  
+  function displayModal() {
+    var modal = document.getElementById('myModal');
+    modal.style.display = "block";
+  }
+  
+     
