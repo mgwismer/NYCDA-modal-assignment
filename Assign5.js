d295ca7c716edb3522482325f14e49d0bf439bb4
Margaret Wismer <margaretgwismer.gmail.com>
2016-11-16 17:57:10 -0500

    Imported images

--- Assign5.js
+++ Assign5.js
@@ -1,8 +1,11 @@
   window.addEventListener("load",setTimeout(displayModal,3000));  
-  
+  document.getElementById('close-button').addEventListener("click",closeModal);
   function displayModal() {
     var modal = document.getElementById('myModal');
-    modal.style.display = "block";
+    modal.style.visibility = "visible";
   }
   
-     
+  function closeModal() {
+  	var modal = document.getElementById('myModal');
+  	modal.style.visibility = "hidden";
+  }
