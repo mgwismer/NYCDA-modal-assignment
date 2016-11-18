e0bae90646ab77921fd01736f8b2abc5ea971c4e
Margaret Wismer <margaretgwismer.gmail.com>
2016-11-18 17:20:18 -0500

    Second modal

--- Assign5.js
+++ Assign5.js
@@ -1,11 +1,23 @@
-  window.addEventListener("load",setTimeout(displayModal,3000));  
-  document.getElementsByClassName('close-button')[0].addEventListener("click",closeModal);
+  waitThreeSeconds();
+
+  function waitThreeSeconds() {
+    window.addEventListener("load",setTimeout(displayModal,3000));
+  }  
+  
   function displayModal() {
     var modal = document.getElementById('myModal');
     modal.style.visibility = "visible";
+    document.getElementsByClassName('modal-content')[0].addEventListener("click",closeModal);
+    document.getElementsByClassName('modal-content')[0].addEventListener("click",openFormModal);
   }
   
   function closeModal() {
   	var modal = document.getElementById('myModal');
   	modal.style.visibility = "hidden";
+  }
+
+  function openFormModal() {
+    closeModal();
+    var modal = document.getElementsByClassName('form-modal')[0];
+    modal.style.visibility = "visible";
   }
